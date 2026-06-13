5ed9e99a63e13ebcbb7ad828fecd343cfc1f503b
guygufygugu-afk <guy.gufy.gugu@gmail.com>
2026-06-13 15:50:37 +0300

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const http = require('http');
-http.createServer((req, res) => res.end("Bot activ!")).listen(process.env.PORT || 3000);
+// Adaugă 'suspect' la comenzile Slash
+const commands = [
+    new SlashCommandBuilder().setName('ping').setDescription('Verifica botul'),
+    new SlashCommandBuilder()
+        .setName('suspect')
+        .setDescription('Marcheaza un utilizator ca suspect de hack')
+        .addUserOption(o => o.setName('user').setDescription('Userul suspect').setRequired(true))
+        .addStringOption(o => o.setName('motiv').setDescription('Motivul').setRequired(true))
+].map(cmd => cmd.toJSON());
 
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// Adaugă logica pentru interacțiunea 'suspect'
+client.on('interactionCreate', async i => {
+    if (!i.isChatInputCommand()) return;
 
-client.once('ready', () => { console.log('Botul e ONLINE!'); });
+    if (i.commandName === 'ping') await i.reply('Pong!');
 
-client.login(process.env.DISCORD_TOKEN);
+    if (i.commandName === 'suspect') {
+        const u = i.options.getUser('user');
+        const motiv = i.options.getString('motiv');
+        const embed = new EmbedBuilder()
+            .setTitle('⚠️ Utilizator Suspect')
+            .setDescription(`Utilizator: ${u}\nMotiv: ${motiv}`)
+            .setColor('#ffff00');
+        await i.reply({ embeds: [embed] });
+    }
+});
